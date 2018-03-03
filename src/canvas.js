f8e4be1354b0ec889918deef1d85d349129a6799
shubham bhawsar <shubhambhawsar63@gmail.com>
2018-03-03 20:45:43 +0530

    3/3/2018

--- src/canvas.js
+++ src/canvas.js
@@ -1,9 +1,9 @@
 // Initial Setup
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 // Variables
 const mouse = {
@@ -15,73 +15,89 @@ const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 
 // Event Listeners
 addEventListener('mousemove', event => {
-    mouse.x = event.clientX
-    mouse.y = event.clientY
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
 })
 
 addEventListener('resize', () => {
-    canvas.width = innerWidth
-    canvas.height = innerHeight
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
 
     init()
 })
 
 // Utility Functions
 function randomIntFromRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function randomColor(colors) {
-    return colors[Math.floor(Math.random() * colors.length)]
+    return colors[Math.floor(Math.random() * colors.length)];
 }
 
 function distance(x1, y1, x2, y2) {
-    const xDist = x2 - x1
-    const yDist = y2 - y1
+    const xDist = x2 - x1;
+    const yDist = y2 - y1;
 
-    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
+    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 }
 
 // Objects
-function Object(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
+function Particle(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.radians=Math.random() * Math.PI*2;
+    this.velocity= .05;
+    this.distanceFromCenter = randomIntFromRange(50,120);
+
+
+    this.update = function() {
+      this.radians+= this.velocity;
+        this.x = x+Math.cos(this.radians)*this.distanceFromCenter;
+        this.y = y+Math.sin(this.radians)*this.distanceFromCenter;
+
+
+        this.draw();
+    }
+
+    this.draw = function() {
+        c.beginPath();
+        c.strokeStyle=this.color;
+        c.lineWidth=this.radius;
+        c.moveTo();
+        c.lineTo();
+        c.stroke();
+        c.closePath();
+    }
+
 }
 
-Object.prototype.update = function() {
-    this.draw()
-}
 
-Object.prototype.draw = function() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-}
 
 // Implementation
-let objects
+let particleArray
 function init() {
-    objects = []
+    particleArray = [];
 
-    for (let i = 0; i < 400; i++) {
-        // objects.push();
+    for (let i = 0; i < 50; i++) {
+        particleArray.push(new Particle(innerWidth/2,innerHeight/2,2,'blue'));
     }
+    console.log(particleArray);
 }
 
 // Animation Loop
 function animate() {
     requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    c.fillStyle='rgba(255,255,255,.05)';
+    c.fillRect(0,0,canvas.width,canvas.height);
 
-    c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-    // objects.forEach(object => {
-    //  object.update();
-    // });
+
+     particleArray.forEach(Particle => {
+      Particle.update();
+     });
 }
 
-init()
-animate()
+init();
+animate();
